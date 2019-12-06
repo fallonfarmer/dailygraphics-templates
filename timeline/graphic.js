ce8b325b675c1a493c7cb36bddfa013dcdb51d17
sarawise <sarawise89@gmail.com>
2019-12-06 15:24:55 -0500

    adds timeline template

---
+++ timeline/graphic.js
@@ -0,0 +1,27 @@
+var pym = require("./lib/pym");
+var ANALYTICS = require("./lib/analytics");
+require("./lib/webfonts");
+
+// If sortable:
+// window.Tablesort = require("tablesort");
+// require("tablesort/dist/sorts/tablesort.number.min");
+// Tablesort(document.querySelector("#state-table"))
+
+pym.then(child => {
+    child.sendHeight();
+
+    child.onMessage("on-screen", function(bucket) {
+        ANALYTICS.trackEvent("on-screen", bucket);
+    });
+    child.onMessage("scroll-depth", function(data) {
+        data = JSON.parse(data);
+        ANALYTICS.trackEvent("scroll-depth", data.percent, data.seconds);
+    });
+
+    window.addEventListener("resize", () => child.sendHeight());
+});
+
+//Add CSS styles inline for the SVG so that they get exported within the SVG file
+$.get("./graphic.css", function(cssContent){
+d3.select(".graphic-wrapper svg g").append("defs").append("style").text(cssContent);
+});
